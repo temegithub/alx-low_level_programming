6e5f498e442a93cc5e855e04598337b25df9993f
temegithub <temesgensamuel2576@gmail.com>
2022-12-10 02:25:13 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -print a c program using p
+ *
+ * Return: 0
+**/
+
+int main(void)
+{
+	printf(""and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
