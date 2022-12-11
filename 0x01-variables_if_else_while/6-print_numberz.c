7d01ea3ef6659dc09931e8c8f74e59300c75fca3
temegithub <temesgensamuel2576@gmail.com>
2022-12-11 00:59:08 -0800

    print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit number of 10 starting from 0
+ * Return: 0
+*/
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
