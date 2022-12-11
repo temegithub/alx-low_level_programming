f2c032822995d834c388a87af0f3e38363ad5393
temegithub <temesgensamuel2576@gmail.com>
2022-12-11 00:32:23 -0800

    print all single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - print all single digit number of base 10 starting from 0
+ * Return: 0
+*/
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
