f6f1e63d324700416b524d7d9037911261af6e8f
temegithub <temesgensamuel2576@gmail.com>
2022-12-13 10:29:32 -0800

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,11 +7,12 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
+int print_lower_digit(int);
 void jack_bauer(void);
 void time_table(void);
 int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
+int print_last_digit(int n);
 
 #endif
