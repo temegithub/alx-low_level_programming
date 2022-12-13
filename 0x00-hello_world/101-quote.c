498b482bbc286dfa9bec433992ebc8cc0608db54
temegithub <temesgensamuel2576@gmail.com>
2022-12-12 18:49:56 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main -print a c program using p
+ * main -Entry
  *
- * Return: 0
+ * Return: 1
 **/
 
 int main(void)
 {
-	printf(("")and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
+
 	return (0);
 }
